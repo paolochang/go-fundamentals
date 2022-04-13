39caa5f83e7e77e00830e08161e5ee4fbff3c9a1
Paolo Chang <pchang@architech.ca>
2022-04-13 15:58:19 -0400

    02e Looping Over Functions

---
+++ 02-Functions/02e-calculator.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+    if len(os.Args) != 3 {
+        fmt.Println("Two integer parameters expected")
+        os.Exit(1)
+    }
+    
+    a, err := strconv.Atoi(os.Args[1])
+    if err != nil {
+        fmt.Println("invalid first argument:", err)
+        os.Exit(1)
+    }
+
+    b, err := strconv.Atoi(os.Args[2])
+    if err != nil {
+        fmt.Println("invalid second argument:", err)
+        os.Exit(1)
+    }
+
+    funcs := []func(int, int) (int, error){
+        adder, subtractor, multiplier, divider,
+    }
+
+    ops := []string{
+        "adding", "subtracting", "multiplying", "dividing",
+    }
+
+    for i, f := range funcs {
+        v, err := f(a, b)
+        if err != nil {
+            fmt.Println(ops[i], "failed:", err)
+        } else {
+            fmt.Println(v)
+        }
+    }
+}
+
+func adder(a, b int) (int, error) {
+    x := a + b
+    if (x > a) != (b > 0) {
+        return x, errors.New("addition out of bounds")
+    }
+    return x, nil
+}
+
+func subtractor(a, b int) (int, error) {
+    x := a - b
+    if (x < a) != (b > 0) {
+        return x, errors.New("subtraction out of bounds")
+    }
+    return x, nil
+}
+
+func multiplier(a, b int) (int, error) {
+    if a == 0 || b == 0 {
+        return 0, nil
+    }
+    x := a * b
+    if x/b != a {
+        return x, errors.New("multiplication out of bounds")
+    }
+    return x, nil
+}
+
+func divider(a, b int) (int, error) {
+    if b == 0 {
+        return 0, errors.New("cannot divide by zero")
+    }
+    return a / b, nil
+}
